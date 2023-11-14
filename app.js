0493c6efed6dead6e849c2763431e6e422e540ff
Sanaullahabbasi <136949915+Sanaullahabbasi@users.noreply.github.com>
2023-11-14 21:21:51 +0500

    passward upgrated

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var newNumPass = "";
 var newCapPass = "";
 var newSmPass = "";
 var newSplPass = "";
+var randomPass;
 
 // console.log("Capital Alphabats==>", capAlp.length, )
 // console.log("Small Alphabats==>", smAlp.length, )
@@ -16,41 +17,46 @@ var newSplPass = "";
 
 for (var i = 0; i < num.length; i++) {
   // newPass = '';
+  randomPass = '';
   var randNum = Math.floor(Math.random() * 10);
   newNumPass += num[randNum];
-  if (newNumPass.length == 1 || newNumPass.length <= 2) {
-    console.log("number", newNumPass);
+  if (newNumPass.length == 1 && newNumPass.length <= 2) {
+    randomPass += newNumPass;
+    }
+  
+}
+for (var i = 0; i < capAlp.length; i++) {
+  // newPass = '';
+  randomPass = '';
+  var randCapAl = Math.floor(Math.random() * 26);
+  newCapPass += capAlp[randCapAl];
+  if (newCapPass.length ==1) {
+    randomPass += newCapPass;
   }
 }
 
-for (var i = 0; i < capAlp.length; i++) {
-  // newPass = '';
-  var randCapAl = Math.floor(Math.random() * 26);
-  newCapPass += capAlp[randCapAl];
-  if (newCapPass.length == 1) {
-    console.log("capital alphabats", newCapPass);
-  }
-}
+
 for (var i = 0; i < smAlp.length; i++) {
   // newPass = '';
+  randomPass = '';
   var randSmAl = Math.floor(Math.random() * 26);
   newSmPass += smAlp[randSmAl];
   if (newSmPass.length > 6 && newSmPass.length <= 8) {
-    console.log("small alphabats", newSmPass);
+    randomPass += newSmPass;
   }
 }
 
 for (var i = 0; i < spChr.length; i++) {
   // newPass = '';
-  var randSplChr = Math.floor(Math.random() * 10);
+  randomPass = '';
+  var randSplChr = Math.floor(Math.random() * 9);
   newSplPass += spChr[randSplChr];
-  if (newSplPass.length === 1 ) {
-    console.log("number", newSplPass);
+  if (newSplPass.length == 1) {
+    randomPass += newSplPass;
   }
 }
 
-var randomPass;
-randomPass = newNumPass + newCapPass + newSmPass + newSplPass;
+// randomPass = newNumPass + newCapPass + newSmPass + newSplPass;
 console.log(randomPass);
 
 // function generate(){
